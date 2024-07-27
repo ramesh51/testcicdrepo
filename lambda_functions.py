8bf284b853e4d72e107104bdf326e669e963aa6d
ramesh51 <rameshreddysurasani@gmail.com>
2024-07-26 21:23:58 -0400

    Update lambda_functions.py

--- lambda_functions.py
+++ lambda_functions.py
@@ -5,6 +5,7 @@ print('Loading function')
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
+    print("ramesh reddy ")
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
